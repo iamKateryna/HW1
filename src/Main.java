ba6a434238a95f0b570c81f2483eae0c04fcd14c
iamKateryna <vadmysigt@gmail.com>
2020-04-10 14:03:03 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        /*System.out.println("Enter two integer numbers");
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println("a + b = " + (a+b));
+        System.out.println("a * b = " + (a*b));
+        System.out.println("a / b = " + (a/b));
+        System.out.println("a - b = " + (a-b));*/
+        /*System.out.println("How are you?");
+        String mood = scanner.nextLine();
+        System.out.println("You are " + mood);*/
+        /*System.out.println("Enter the radius of the bed to calculate its perimeter?");
+        double r = scanner.nextDouble();
+        double pi = 3.14159;
+        double p = 2*r * pi;
+        System.out.println(p);*/
+        /*System.out.println("What is your name?");
+        String name = scanner.nextLine();
+        System.out.println("Where do you live, " + name + "?");
+        String adress = scanner.nextLine();
+        System.out.println(name + " lives in " + adress);*/
+        /*System.out.println("Enter the price of calls to all three countries per minute");
+        System.out.print("c1 = ");
+        double c1 = scanner.nextDouble();
+        System.out.print("c2 = ");
+        double c2 = scanner.nextDouble();
+        System.out.print("c3 = ");
+        double c3 = scanner.nextDouble();
+        System.out.println("Enter the duration of all calls");
+        System.out.print("t1 = ");
+        double t1 = scanner.nextDouble();
+        System.out.print("t2 = ");
+        double t2 = scanner.nextDouble();
+        System.out.print("t3 = ");
+        double t3 = scanner.nextDouble();
+        System.out.println("The price of the 1st call is " + (c1*t1));
+        System.out.println("The price of the 2nd call is " + (c2*t2));
+        System.out.println("The price of the 3rd call is " + (c3*t3));
+        System.out.println("Total price is " + (c1*t1+c2*t2+c3*t3));*/
+
+    }
+}
